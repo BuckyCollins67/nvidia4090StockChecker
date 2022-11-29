1b7eda8e37be7545d455094eaeea9a8a5667dc93
Bucky Collins <bucky.collins.67@gmail.com>
2022-11-28 20:33:47 -0800

    successfully able to access site.

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const puppeteer = require('puppeteer')
+const fs = require('fs/promises')
 
 async function start(){
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
+    await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36")
     await page.goto("https://store.nvidia.com/en-us/geforce/store/?page=1&limit=9&locale=en-us&search=4090")
     await page.screenshot({path: "4090WebsiteQuery.png"})
+
+    const names = await page.evaluate(() => {
+        return Array.from(document.querySelector("#resultsDiv > div > div:nth-child(1) > div.product-details-grid-overview.\31 20 > div.clearfix.pdc-139.fe-pids > a")).map(x => x.textContent)
+    })
+    await fs.writeFile("names.txt", names.join("\r\n"))
+
     await browser.close()
 }
 
